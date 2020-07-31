0effd6b553dc44918de3abb5749e27de0af3e6eb
Evan Feder <39915430+EvanFeder@users.noreply.github.com>
2020-07-30 22:49:49 -0400

    Update hello_aliens.R

--- hello_aliens.R
+++ hello_aliens.R
@@ -1,2 +1,2 @@
-message = "(Hoitines pot'este chairete! Eirēnikōs pros philous elēlythamen philoi.). \n Paz e felicidade a todos.\n Gok wai hou maa? Zuk gok wai ping on gin hong faai lok.	\n Adanniš lu šulmu. \n (Sàwàtdee kâ, sàhăai nai torránee póhn. Pûak rao nai torránee née kŏr sòng mít jìt maa tĕung tâan túk kon). \n Bonjour tout le monde. Hello from the children of planet Earth."
+message = "Bonjour tout le monde. \n Hello from the children of planet Earth. We welcome you fully and would like to learn more about your society. Please communicate back."
 cat(message)
